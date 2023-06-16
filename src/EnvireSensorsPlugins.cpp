5e93e3ed48527f9cbea0be5e3f92dc64fb877dff
Anna Born <anna.born@dfki.de>
2023-06-16 10:06:44 +0200

    Initial commit

---
+++ src/EnvireSensorsPlugins.cpp
@@ -0,0 +1,145 @@
+/**
+ * \file EnvireMotorsPlugins.cpp
+ * \author Malte Langosz
+ *
+ */
+
+#include "EnvireSensorsPlugins.hpp"
+
+//#include "PhysicsMapper.h"
+
+#include <mars/utils/mathUtils.h>
+#include <mars/utils/misc.h>
+#include <mars_interfaces/graphics/GraphicsManagerInterface.h>
+#include <mars_interfaces/sim/LoadCenter.h>
+#include <mars_interfaces/sim/LoadSceneInterface.h>
+#include <mars_interfaces/sim/JointInterface.h>
+#include <lib_manager/LibInterface.hpp>
+#include <lib_manager/LibManager.hpp>
+
+#include <mars_interfaces/Logging.hpp>
+
+#include <mars_interfaces/MotorData.h>
+#include <mars_interfaces/sim/SimulatorInterface.h>
+#include <mars_interfaces/sim/SensorManagerInterface.h>
+
+#define SIM_CENTER_FRAME_NAME "world"
+typedef envire::core::GraphTraits::vertex_descriptor VertexDesc;
+
+namespace mars
+{
+    namespace envire_motors
+    {
+
+        using std::string;
+        using namespace utils;
+        using namespace interfaces;
+        using namespace configmaps;
+
+        EnvireMotorsPlugins::EnvireMotorsPlugins(lib_manager::LibManager *theManager) :
+            lib_manager::LibInterface(theManager)
+        {
+            GraphItemEventDispatcher<envire::core::Item<::envire::base_types::sensors::CameraSensor>>::subscribe(ControlCenter::envireGraph.get());
+            GraphItemEventDispatcher<envire::core::Item<::envire::base_types::sensors::RaySensor>>::subscribe(ControlCenter::envireGraph.get());
+        }
+
+        EnvireMotorsPlugins::~EnvireMotorsPlugins()
+        {
+        }
+
+        // TODO: this should be moved out from here
+        std::shared_ptr<SubControlCenter> EnvireMotorsPlugins::getControlCenter(envire::core::FrameId frame)
+        {
+            // search for physics interface in graph
+            bool done = false;
+            while(!done)
+            {
+                const envire::core::GraphTraits::vertex_descriptor vertex = ControlCenter::envireGraph->vertex(frame);
+                envire::core::GraphTraits::vertex_descriptor parentVertex = ControlCenter::graphTreeView->tree[vertex].parent;
+                // todo: check if this check is correct
+                if(parentVertex)
+                {
+                    frame = ControlCenter::envireGraph->getFrameId(parentVertex);
+                    try
+                    {
+                        using SubControlItem = envire::core::Item<std::shared_ptr<SubControlCenter>>;
+                        envire::core::EnvireGraph::ItemIterator<SubControlItem> it = ControlCenter::envireGraph->getItem<SubControlItem>(frame);
+                        return it->getData();
+                    }
+                    catch (...)
+                    {
+                    }
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+            return nullptr;
+        }
+
+        void EnvireMotorsPlugins::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::base_types::sensors::CameraSensor>>& e)
+        {
+            std::shared_ptr<interfaces::SubControlCenter> subControl = getControlCenter(frameId);
+            if (subControl == nullptr)
+                return;
+
+            envire::base_types::sensors::CameraSensor& sensor = e.item->getData();
+            ConfigMap config = sensor.getFullConfigMap();
+
+            config["name"] = subControl->getPrefix() + config["name"].getString();
+
+            unsigned long drawID = control->graphics->getDrawID(e.frame);
+            if(drawID)
+            {
+                config["draw_id"] = drawID;
+
+                // TODO: temporarly add base sensor into the graph
+                // we can replace it with the similar structure as DynamicObjectItem: BaseSensorItem
+                std::shared_ptr<interfaces::BaseSensor> baseSensor;
+                baseSensor.reset(control->sensors->createAndAddSensor(&config));
+                envire::core::Item<std::shared_ptr<interfaces::BaseSensor>>::Ptr baseSensorItemPtr(new envire::core::Item<std::shared_ptr<interfaces::BaseSensor>>(baseSensor));
+                ControlCenter::envireGraph->addItemToFrame(e.frame, baseSensorItemPtr);
+            }
+        }
+
+        void EnvireMotorsPlugins::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::base_types::sensors::RaySensor>>& e)
+        {
+            std::shared_ptr<interfaces::SubControlCenter> subControl = getControlCenter(frameId);
+            if (subControl == nullptr)
+                return;
+
+            envire::base_types::sensors::RaySensor& sensor = e.item->getData();
+            ConfigMap config = sensor.getFullConfigMap();
+
+            config["name"] = subControl->getPrefix() + config["name"].getString();
+
+            // get parent smurf frame
+            const envire::core::GraphTraits::vertex_descriptor vertex = ControlCenter::envireGraph->vertex(e.frame);
+            envire::core::GraphTraits::vertex_descriptor parentVertex = ControlCenter::graphTreeView->tree[vertex].parent;
+            envire::core::FrameId parentFrame = ControlCenter::envireGraph->getFrameId(parentVertex);
+
+            bool found = false;
+            config["groupName"] = "mars_sim";
+            config["dataName"] = "Frames/"+parentFrame;
+            envire::core::Transform t = ControlCenter::envireGraph->getTransform(SIM_CENTER_FRAME_NAME, parentFrame);
+            vectorToConfigItem((&config["init_position"]), &(t.transform.translation));
+            quaternionToConfigItem(&(config["init_orientation"]), &(t.transform.orientation));
+            t = ControlCenter::envireGraph->getTransform(parentFrame, e.frame);
+            vectorToConfigItem(&(config["pos_offset"]), &(t.transform.translation));
+            quaternionToConfigItem(&(config["ori_offset"]), &(t.transform.orientation));
+
+            // TODO: temporarly add base sensor into the graph
+            // we can replace it with the similar structure as DynamicObjectItem: BaseSensorItem
+            std::shared_ptr<interfaces::BaseSensor> baseSensor;
+            baseSensor.reset(control->sensors->createAndAddSensor(&config));
+            envire::core::Item<std::shared_ptr<interfaces::BaseSensor>>::Ptr sensorItemPtr(new envire::core::Item<std::shared_ptr<interfaces::BaseSensor>>(baseSensor));
+            ControlCenter::envireGraph->addItemToFrame(e.frame, sensorItemPtr);
+        }
+
+    } // end of namespace envire_sensors
+
+} // end of namespace mars
+
+DESTROY_LIB(mars::envire_motors::EnvireMotorsPlugins);
+CREATE_LIB(mars::envire_motors::EnvireMotorsPlugins);
---
+++ src/EnvireSensorsPlugins.hpp
@@ -0,0 +1,66 @@
+/**
+ * \file EnvireOdePhysicsPlugins.hpp
+ * \author Malte Langosz
+ * \brief Plugin class to load physics representation based on envire items
+ *
+ */
+
+#pragma once
+
+// TODO: check and clean the header includes
+
+#include <mars_interfaces/sim/ControlCenter.h>
+#include <mars/utils/Vector.h>
+
+#include <lib_manager/LibInterface.hpp>
+
+#include <envire_core/items/Item.hpp>
+#include <envire_core/graph/EnvireGraph.hpp>
+#include <envire_core/graph/TreeView.hpp>
+#include <envire_core/events/GraphEventDispatcher.hpp>
+#include <envire_core/events/GraphItemEventDispatcher.hpp>
+
+#include <envire_base_types/sensors/CameraSensor.hpp>
+#include <envire_base_types/sensors/RaySensor.hpp>
+
+#include <iostream>
+
+namespace mars
+{
+    namespace envire_sensors
+    {
+        // move the typedef to separate file
+        class EnvireSensorsPlugins : public lib_manager::LibInterface,
+                                    public envire::core::GraphEventDispatcher,
+                                    public envire::core::GraphItemEventDispatcher<envire::core::Item<::envire::base_types::sensors::CameraSensor>>,
+                                    public envire::core::GraphItemEventDispatcher<envire::core::Item<::envire::base_types::sensors::RaySensor>>
+        {
+
+        public:
+            EnvireSensorsPlugins(lib_manager::LibManager *theManager); ///< Constructor of the \c class Simulator.
+            virtual ~EnvireSensorsPlugins();
+
+            // --- LibInterface ---
+            int getLibVersion() const override
+            {
+                return 1;
+            }
+
+            const std::string getLibName() const override
+            {
+                return std::string("envire_mars_motors");
+            }
+
+            CREATE_MODULE_INFO();
+
+            virtual void itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::base_types::sensors::CameraSensor>>& e) override;
+            virtual void itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::base_types::sensors::RaySensor>>& e) override;
+
+        private:
+            std::shared_ptr<interfaces::SubControlCenter> getControlCenter(envire::core::FrameId frame);
+
+            void createSensor(configmaps::ConfigMap &config, const std::string &frameId);
+        };
+
+    } // end of namespace envire_sensors
+} // end of namespace mars
